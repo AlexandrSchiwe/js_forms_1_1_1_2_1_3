5a63414250f5968059837454be30ffa1dbfa56b1
githubsupercode <alex.supercode@gmail.com>
2022-04-20 12:24:15 +0200

    lastpush

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// JS Einführung Forms 1.1 double number
+
+// let inputValue = document.getElementById("value");
+// let output = document.getElementById("output");
+
+// function doubleNumber() {
+//     output.innerHTML = inputValue.value * 2;
+// }
+
+// JS Einführung Forms 1.2 birthday
+
+// let inputValue = document.getElementById("value");
+// let output = document.getElementById("output"); 
+
+// function birthday() {
+//     output.innerHTML = (2022) - inputValue.value;
+// };
+
+// JS Einführung 1.3 
+
+let num1 = document.getElementById("value1");
+let num2 = document.getElementById("value2");
+let output = document.getElementById("output"); 
+
+function difference() {
+    output.innerHTML = num1.value - num2.value;
+};
+
+// Fehler in der console: vergessen value2 eine ID zu geben.
